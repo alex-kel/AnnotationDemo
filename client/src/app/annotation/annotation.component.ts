9979e5c22a7217b2e04439ddc83fcc10d84ba225
Alexander Kel <1906.kas@gmail.com>
2017-05-18 01:10:45 +0300

    Finished base implementation

--- client/src/app/annotation/annotation.component.ts
+++ client/src/app/annotation/annotation.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {AnnotatedCase} from '../annotated-case';
+import {AnnotatedCase, Annotation, Offset} from '../annotated-case';
 
 @Component({
   selector: 'app-annotation',
@@ -13,16 +13,37 @@ export class AnnotationComponent implements OnInit {
 
   ngOnInit() { }
 
+  // This approach works fine but fails on Regex creation in few cases, depending on annotation text
+  // annotationClicked(id: String) {
+  //   const annotatonText: string = this.annotatedCase.annotations
+  //     .find(annotation => annotation.id === id)
+  //     .text;
+  //   const query = new RegExp('(\\b' + annotatonText + '\\b)', 'gim');
+  //   const e: string = document.getElementById('casetext').innerHTML;
+  //   const enew: string = e.replace(/(<span>|<\/span>)/igm, '');
+  //   document.getElementById('casetext').innerHTML = enew;
+  //   const newe: string = enew.replace(annotatonText, `<span>${annotatonText}</span>`);
+  //   document.getElementById('casetext').innerHTML = newe;
+  // }
+
   annotationClicked(id: String) {
-    const annotatonText: string = this.annotatedCase.annotations
-      .find(annotation => annotation.id === id)
-      .text;
-    const query = new RegExp('(\\b' + annotatonText + '\\b)', 'gim');
-    const e: string = document.getElementById('casetext').innerHTML;
-    const enew: string = e.replace(/(<span>|<\/span>)/igm, '');
-    document.getElementById('casetext').innerHTML = enew;
-    const newe: string = enew.replace(annotatonText, `<span>${annotatonText}</span>`);
-    console.log(newe);
-    document.getElementById('casetext').innerHTML = newe;
+    let text: string = this.annotatedCase.text;
+    const annotation: Annotation = this.annotatedCase.annotations
+      .find(ann => ann.id === id);
+    text = text.replace(/<span>/g, '')
+      .replace(/<\/span>/g, '');
+
+    let currentShift = 0;
+    for (const offset of annotation.offsets) {
+      const start = offset.start + currentShift;
+      text = [text.slice(0, start), '<span>', text.slice(start)].join('');
+      currentShift += '<span>'.length;
+
+      const end = offset.end + currentShift;
+      text = [text.slice(0, end), '</span>', text.slice(end)].join('');
+      currentShift += '</span>'.length;
+    }
+
+    this.annotatedCase.text = text;
   }
 }
--- client/src/app/text/text.component.ts
+++ client/src/app/text/text.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {AnnotatedCase} from '../annotated-case';
 
 @Component({
   selector: 'app-text',
   templateUrl: './text.component.html',
-  styleUrls: ['./text.component.css']
+  styleUrls: ['./text.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TextComponent implements OnInit {
   @Input() annotatedCase: AnnotatedCase;
